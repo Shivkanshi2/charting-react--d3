a7f39007e57141410a195b9d7b4ce21cb4685c26
Shivkanshi2 <130892499+Shivkanshi2@users.noreply.github.com>
2024-10-22 16:42:33 +0530

    Update BarChart.jsx

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
@@ -7,13 +6,13 @@ const BarChart = ({
   width,
   height,
   xAccessor,
-  yAccessor,
+  yAccessors,
   dimensions,
   measures,
   tooltipFormat,
   annotations,
   xAxisLabel,
-  yAxisLabel,
+  yAxisLabels,
   colors,
   onHover,
 }) => {
@@ -25,7 +24,7 @@ const BarChart = ({
       .attr('height', height);
 
     svg.selectAll('*').remove();
-    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+    const margin = { top: 20, right: 100, bottom: 40, left: 100 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
     const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
@@ -33,70 +32,100 @@ const BarChart = ({
     const xScale = d3.scaleBand()
       .domain(data.map(xAccessor))
       .range([0, innerWidth])
-      .padding(0.1);
+      .padding(0.2); 
 
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data, yAccessor)])
-      .nice()
-      .range([innerHeight, 0]);
+    const groupWidth = xScale.bandwidth() / yAccessors.length;
 
-    g.selectAll('.bar')
-      .data(data)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => xScale(xAccessor(d)))
-      .attr('y', d => yScale(yAccessor(d)))
-      .attr('width', xScale.bandwidth())
-      .attr('height', d => innerHeight - yScale(yAccessor(d)))
-      .attr('fill', (d, i) => colors[i % colors.length])
-      .on('mouseover', (event, d) => {
-        if (onHover) onHover(d);
-        const tooltip = d3.select('#tooltip')
-          .style('visibility', 'visible')
-          .text(tooltipFormat(d));
-      })
-      .on('mousemove', (event) => {
-        d3.select('#tooltip')
-          .style('top', (event.pageY - 10) + 'px')
-          .style('left', (event.pageX + 10) + 'px');
-      })
-      .on('mouseout', () => {
-        d3.select('#tooltip').style('visibility', 'hidden');
-      });
+    const yScales = yAccessors.map((_, i) => 
+      d3.scaleLinear()
+        .domain([0, d3.max(data, d => d[yAccessors[i]])])
+        .nice()
+        .range([innerHeight, 0])
+    );
 
+    
+    yAccessors.forEach((yAccessor, i) => {
+      g.selectAll(`.bar-${i}`)
+        .data(data)
+        .enter().append('rect')
+        .attr('class', `bar-${i}`)
+        .attr('x', d => xScale(xAccessor(d)) + i * (groupWidth + 10)) 
+        .attr('y', d => yScales[i](d[yAccessor]))
+        .attr('width', groupWidth - 5) // Width of each bar
+        .attr('height', d => innerHeight - yScales[i](d[yAccessor]))
+        .attr('fill', colors[i % colors.length])
+        .on('mouseover', (event, d) => {
+          if (onHover) onHover(d);
+          const tooltip = d3.select('#tooltip')
+            .style('visibility', 'visible')
+            .text(tooltipFormat(d));
+        })
+        .on('mousemove', (event) => {
+          d3.select('#tooltip')
+            .style('top', (event.pageY - 10) + 'px')
+            .style('left', (event.pageX + 10) + 'px');
+        })
+        .on('mouseout', () => {
+          d3.select('#tooltip').style('visibility', 'hidden');
+        });
+    });
+
+    
     g.append('g')
       .attr('class', 'x-axis')
       .attr('transform', `translate(0,${innerHeight})`)
       .call(d3.axisBottom(xScale))
       .append('text')
       .attr('x', innerWidth)
-      .attr('y', -6)
-      .attr('fill', 'white')
+      .attr('y', 30)
+      .attr('fill', 'black') 
       .attr('text-anchor', 'end')
       .text(xAxisLabel);
 
-    g.append('g')
-      .attr('class', 'y-axis')
-      .call(d3.axisLeft(yScale))
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '0.71em')
-      .attr('fill', 'white')
-      .attr('text-anchor', 'end')
-      .text(yAxisLabel);
+    
+    yScales.forEach((yScale, i) => {
+      const yAxisLeft = g.append('g')
+        .attr('class', `y-axis-left-${i}`)
+        .attr('transform', `translate(${(groupWidth + 10) * i + 5}, 0)`) 
+        .call(d3.axisLeft(yScale));
 
+      yAxisLeft.append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', -10)
+        .attr('dy', '0.71em')
+        .attr('fill', 'black') 
+        .attr('text-anchor', 'end')
+        .text(yAxisLabels[i]);
+    });
+
+    
+    yScales.forEach((yScale, i) => {
+      const yAxisRight = g.append('g')
+        .attr('class', `y-axis-right-${i}`)
+        .attr('transform', `translate(${innerWidth - (groupWidth + 10) * i - 10}, 0)`) 
+        .call(d3.axisRight(yScale));
+
+      yAxisRight.append('text')
+        .attr('transform', 'rotate(90)')
+        .attr('y', 10)
+        .attr('dy', '0.71em')
+        .attr('fill', 'black') 
+        .attr('text-anchor', 'end')
+        .text(yAxisLabels[i]);
+    });
+
+    
     if (annotations) {
       annotations.forEach(annotation => {
         g.append('text')
           .attr('x', xScale(annotation.x))
-          .attr('y', yScale(annotation.y))
+          .attr('y', yScales[0](annotation.y)) 
           .attr('dy', '-0.5em')
           .attr('fill', 'red')
           .text(annotation.label);
       });
     }
-  }, [data, width, height, xAccessor, yAccessor, xAxisLabel, yAxisLabel, colors, annotations]);
+  }, [data, width, height, xAccessor, yAccessors, xAxisLabel, yAxisLabels, colors, annotations]);
 
   return (
     <>
